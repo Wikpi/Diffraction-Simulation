1a99d748afa006f7342cc36f046934b330c0b75f
Anand Sharma <connect2anands@gmail.com>
2025-06-18 09:26:33 +0200

    Add meas data (#4)

--- util/data.py
+++ util/data.py
@@ -4,7 +4,15 @@ from numpy.typing import NDArray
 ### Data path constants
 
 # Diffraction data for 79e-5 slit width 
-dataPath: str = "data/diffraction_data_79e-5.dat"
+# dataPath: str = "data/diffraction_data_79e-5.dat"
+dataPath620: str = "data/20250617_diffraction_meas1_620.dat"
+dataPath650: str = "data/20250617_diffraction_meas2_650.dat"
+dataPath700: str = "data/20250617_diffraction_meas3_700.dat"
+dataPath750: str = "data/20250617_diffraction_meas4_750.dat"
+dataPath800: str = "data/20250617_diffraction_meas5_800.dat"
+dataPath850: str = "data/20250617_diffraction_meas6_850.dat"
+dataPath900: str = "data/20250617_diffraction_meas7_900.dat"
+dataPath950: str = "data/20250617_diffraction_meas8_950.dat"
 
 ## Data function
 
